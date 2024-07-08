98214bd5e432986319133d21949eb873bb351e25
Your Name <your.email@example.com>
2024-07-09 01:56:22 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://unithinkportal.netlify.app/' // Replace with your frontend URL
+  origin: 'https://unithinkportal.netlify.app' // Replace with your frontend URL
 }));
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
